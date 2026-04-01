a0a7a720173717f995218f636647054a77db960b
Sautrik <sr9973@srmist.edu.in>
2026-04-02 04:09:47 +0530

    chore: remove unused internal scripts and initial Next.js boilerplate files

--- test-models.js
+++
@@ -1,7 +0,0 @@
-async function run() {
-  const key = process.env.GEMINI_API_KEY;
-  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`);
-  const data = await res.json();
-  data.models.forEach(m => console.log(m.name));
-}
-run();
